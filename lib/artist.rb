4a0cf88e0dac009a0152e346adbdef507f85f87d
Allie <alliecaton3@gmail.com>
2020-12-22 13:25:53 -0500

    commit tree 4833961aa9726b3345de5f382882ae0fbba96f06

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,38 @@
+require 'pry'
+class Artist 
 
+attr_accessor :name
+
+@@all =[]
+
+    def initialize(name)
+        @name = name 
+        save
+    end 
+
+    def save 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        song
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre
+        end 
+    end
+
+end 
